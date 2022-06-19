f8175603c4c3b2cdc32930086d7413f666a95993
Samuel <samuelschanderl@Samuels-MacBook-Pro.local>
2022-06-19 22:14:02 +0200

    wip - improved js functionality

--- script.js
+++ script.js
@@ -1,97 +1,62 @@
 const output = document.getElementById('output')
 const buttons = document.getElementsByClassName('button')
 
-// get each number value and parse to an integer --> check if necessary
-const one = parseInt(document.getElementById('1'))
-const two = parseInt(document.getElementById('2'))
-const three = parseInt(document.getElementById('3'))
-const four = parseInt(document.getElementById('4'))
-const five = parseInt(document.getElementById('5'))
-const six = parseInt(document.getElementById('6'))
-const seven = parseInt(document.getElementById('7'))
-const eight = parseInt(document.getElementById('8'))
-const nine = parseInt(document.getElementById('9'))
-const zero = parseInt(document.getElementById('0'))
-
 // set the variables
-let clickedValue = 0
-let lastValue = 0
-let sum = 0
-let operator = ''
+let calculation = ''
+let currentNumber = ''
+
+console.log(eval('1+2*2'))
 
 // invoke the event listener to add the 
 Array.from(buttons).forEach((button) => {
 
-
-
     button.addEventListener('click', () => { 
 
-        // check if button contains number, parse from string to int
-        if (button.classList.contains('number')) {
-            lastValue = clickedValue
-            clickedValue = parseInt(button.innerHTML)
+        console.log(calculation)
 
-            console.log(clickedValue, lastValue)
+        // check if button contains number and add values
+        if (button.classList.contains('number')){
+    
+            // add the values to the empy string
+            currentNumber = currentNumber.concat(button.innerHTML)
 
-            // display the output
-            output.innerText = clickedValue
+            // output the value you clicked in the output field
+            output.innerHTML = currentNumber
+        }
+        // check if button contains operator
+        else if (button.classList.contains('operator')) {
+                // add the current Number to the calculation string before adding the operator
+                calculation =  calculation.concat(currentNumber, button.innerHTML)
+                currentNumber = ''
+                output.innerHTML = button.innerHTML
         }
 
-        else if (button.id === 'add') {
-            console.log('add')
-            sum = add(clickedValue, lastValue)
-            output.innerText = button.innerHTML
+        // TODO - Add the exponent correctly to the string
+        else if (button.id === 'exponent') {
+            calculation = calculation.concat('**')
+            console.log(calculation)
+            output.innerHTML = currentNumber
         }
-
-        else if (button.id === 'subtract') {
-            console.log('minus')
-            output.innerText = button.innerHTML
-        }
-
-        else if (button.id === 'multiply') {
-            console.log('multiply')
-            output.innerText = button.innerHTML
-        }
-
-        else if (button.id === 'divide') {
-            console.log('divide')
-            output.innerText = button.innerHTML
+            
+        else if (button.id === 'equals') {
+            // add the current Number to the calculation string before adding the operator
+            calculation =  calculation.concat(currentNumber)
+            // convert the string to an equation and evaluate it
+            calculatedResult = eval(calculation)
+            output.innerText = calculatedResult
+            // reset the values
+            calculation = currentNumber = ''
         }
 
         else if (button.id === 'clear') {
-            let clickedValue = 0
-            let lastValue = 0
-            let sum = 0
-            output.innerText = clickedValue
+            // reset the values
+            calculation = currentNumber = ''
+            output.innerText = 0
         }
 
-        // get the final output to the screen
-        else if (button.id === 'equals') {
-            console.log('equals')
-            output.innerText = sum
+        else if (button.id === 'switch') {
+            currentNumber = eval(currentNumber) * -1
+            output.innerHTML = currentNumber
         }
-    })
-})
 
-function add(val1, val2) {
-    let sum = val1 + val2
-    return sum
-}
-
-function subtract(val1, val2) {
-    let sum = val1 - val2
-    return sum
-}
-
-function multiply(val1, val2) {
-    let sum = val1 * val2
-    return sum
-}
-
-function divide(val1, val2) {
-    let sum = val1 / val2
-    return sum
-}
-
-const result = multiply(10,2)
-console.log(result)
+})})
