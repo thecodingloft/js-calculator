599584d71e650f7b025b2e85a7bee8e7bf814e0a
Samuel <samuelschanderl@Samuels-MacBook-Pro.local>
2022-06-20 23:35:27 +0200

    wip - check for last operator, switch if needed

--- script.js
+++ script.js
@@ -13,10 +13,11 @@ Array.from(buttons).forEach((button) => {
     button.addEventListener('click', () => { 
 
         console.log(calculation)
+        console.log(calculation.charAt(calculation.length - 1))
 
         // check if button contains number and add values
         if (button.classList.contains('number')){
-    
+            console.log(calculation)
             // add the values to the empy string
             currentNumber = currentNumber.concat(button.innerHTML)
 
@@ -25,22 +26,29 @@ Array.from(buttons).forEach((button) => {
         }
         // check if button contains operator
         else if (button.classList.contains('operator')) {
+                if (currentNumber) {
                 // add the current Number to the calculation string before adding the operator
                 calculation =  calculation.concat(currentNumber, button.innerHTML)
                 currentNumber = ''
                 output.innerHTML = button.innerHTML
+                } else if (calculation.charAt(calculation.length - 1).match(/\+-\*\^\//)) {
+                    console.log('regex')
+                }
         }
 
         // TODO - Add the exponent correctly to the string
         else if (button.id === 'exponent') {
-            calculation = calculation.concat('**')
+            calculation =  calculation.concat(currentNumber, '**')
+            currentNumber = ''
             console.log(calculation)
-            output.innerHTML = currentNumber
+            output.innerHTML = button.innerHTML
         }
             
         else if (button.id === 'equals') {
+            
             // add the current Number to the calculation string before adding the operator
             calculation =  calculation.concat(currentNumber)
+            console.log(calculation)
             // convert the string to an equation and evaluate it
             calculatedResult = eval(calculation)
             output.innerText = calculatedResult
